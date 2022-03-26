c9490e2ce43c70e0786b444581a8f6c793dba294
Joe <hutcheson2001@yahoo.com>
2022-03-26 08:29:21 -0400

    Set up main components

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header"
+import Search from "./Search"
+import NewsWindow from "./NewsWindow"
+
+function App(){
+  return (
+    <>
+      <h1>This is App</h1>
+      <Header/>
+      <Search/>
+      <NewsWindow/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NewsWindow.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function NewsWindow(){
+  return(
+    <div>This is NewsWindow</div>
+  )
+}
+
+export default NewsWindow
